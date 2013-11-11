fbea02a4b14a3529f82ddb8e45a7c75f3e3263d6
schmidtbene <schmidtbene@DEWNGCZ2XN1.de-wn.sankt-wendel.germany.fresenius.de>
2013-11-11 15:09:19 +0100

    Aktualisierung der Summary live bei Änderung der Werte

--- madPracticeEclipse/src/mobile/app/dev/ueb03/SettingsActivity.java
+++ madPracticeEclipse/src/mobile/app/dev/ueb03/SettingsActivity.java
@@ -1,22 +1,53 @@
 package mobile.app.dev.ueb03;
 
 import mobile.app.dev.R;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 
-public class SettingsActivity extends PreferenceActivity {
+@SuppressWarnings("deprecation")
+public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 
-	@SuppressWarnings("deprecation")
+	private EditTextPreference fontSizePreference;
+	
 	@Override
 	protected void onCreate(Bundle instanceState) {
 		super.onCreate(instanceState);
 
 		addPreferencesFromResource(R.xml.preferences);
+
+		fontSizePreference = (EditTextPreference) getPreferenceScreen().findPreference("fontSize");
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		fontSizePreference.setSummary(prefs.getString("fontSize", "20"));
+		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		return false;
 	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		if(key.equals("fontSize")){
+			fontSizePreference.setSummary(sharedPreferences.getString(key, "20"));
+		}
+		
+	}
+	
 }
