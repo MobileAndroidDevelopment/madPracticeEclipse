d147e14aa0d077f634444ef837e7184e18e11bc9
Benne <benediktschmidt89@gmail.com>
2013-11-23 10:56:17 +0100

    DownloaderService umgestellt, Activity kann Service nun beim Button-Klick starten Strings ausgelagert

--- madPracticeEclipse/src/mobile/app/dev/ueb04/DownloadActivity.java
+++ madPracticeEclipse/src/mobile/app/dev/ueb04/DownloadActivity.java
@@ -2,8 +2,11 @@ package mobile.app.dev.ueb04;
 
 import mobile.app.dev.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class DownloadActivity extends Activity {
 
@@ -18,6 +21,13 @@ public class DownloadActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.download, menu);
 		return true;
+	}
+
+	public void startDownload(View view) {
+		String url = ((EditText) findViewById(R.id.editTextDownloadLink)).getText().toString();
+		Intent intent = new Intent(this, DownloaderService.class);
+		intent.putExtra(DownloaderService.URL_KEY, url);
+		startService(intent);
 	}
 
 }
--- madPracticeEclipse/src/mobile/app/dev/ueb04/DownloaderService.java
+++ madPracticeEclipse/src/mobile/app/dev/ueb04/DownloaderService.java
@@ -1,25 +1,77 @@
 package mobile.app.dev.ueb04;
 
-import android.app.Service;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.app.IntentService;
 import android.content.Intent;
-import android.os.Binder;
-import android.os.IBinder;
+import android.os.Environment;
+import android.util.Log;
 
-public class DownloaderService extends Service {
+public class DownloaderService extends IntentService {
 
-	private final IBinder binder = new LocalBinder();
-	
-	public class LocalBinder extends Binder{
-		
-		public DownloaderService getService(){
-			return DownloaderService.this;
+	public static final String URL_KEY = "URL";
+	private static final String TITLE = "File-Download";
+
+	public DownloaderService() {
+		super(TITLE);
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		String downloadUrl = intent.getStringExtra(URL_KEY);
+		InputStream input = null;
+		OutputStream output = null;
+		HttpURLConnection connection = null;
+		try {
+			URL url = new URL(downloadUrl);
+			connection = (HttpURLConnection) url.openConnection();
+			connection.connect();
+
+			// expect HTTP200
+			if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+				Log.e("DOWNLOAD", "keine Connection!");
+				// TODO: Fehlerhandling
+			}
+
+			// this will be useful to display download percentage
+			// might be -1: server did not report the length
+			int fileLength = connection.getContentLength();
+
+			// download the file
+			input = connection.getInputStream();
+			output = new FileOutputStream(Environment.getExternalStorageDirectory().getPath()+"/file_name.extension");
+
+			byte data[] = new byte[4096];
+			long total = 0;
+			int count;
+			while ((count = input.read(data)) != -1) {
+				total += count;
+				// publishing the progress....
+				if (fileLength > 0) { // only if total length is known
+				//                        onProgressUpdate((int) (total * 100 / fileLength));
+					;
+					// TODO: Download-Status publishen
+				}
+				output.write(data, 0, count);
+			}
+		} catch (Exception e) {
+			Log.e("DOWNLOAD", e.getMessage());
+		} finally {
+			try {
+				if (output != null)
+					output.close();
+				if (input != null)
+					input.close();
+			} catch (IOException ignored) {
+			}
+
+			if (connection != null)
+				connection.disconnect();
 		}
 	}
-	
-	@Override
-	public IBinder onBind(Intent intent) {
-		return binder;
-	}
-
-	// TODO: Download starten etc
 }
